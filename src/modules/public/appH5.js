cf6e87a510e23b20d2af3567eac0d3bdd1006ef5
Ayerss <276956101@qq.com>
2016-09-12 11:13:52 +0800

    V1.0
    改用MVC框架，修复大屏下全屏bug

--- src/modules/public/appH5.js
+++ src/modules/public/appH5.js
@@ -5,41 +5,46 @@
  * @Desc : 公共js
  */
 
-/**
- * @desc : 将字符串转换为对象
- *
- */
 (function (doc, win) {
     var docEl = doc.documentElement,
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function () {
             var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            docEl.style.fontSize = 20 * (clientWidth / 360) + 'px';
+            if(clientWidth > 360){
+              docEl.style.fontSize = '20px';
+            }else {
+              if (!clientWidth) return;
+              docEl.style.fontSize = 20 * (clientWidth / 360) + 'px';
+            }
         };
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
+
+    window.onload = function () {
+        var h = document.querySelectorAll('#head a img')[0].height;
+        document.getElementById('body').style.paddingTop = h+'px';
+    };
+
+    var $OH = {
+        /**
+         * @desc : 获取url上参数值
+         * @access : public
+         * @param : string
+         * @return : object / null
+         */
+        getQueryStr : function (n) {
+            var a = new RegExp("(^|&)"+ n +"=([^&]*)(&|$)");
+            var r = window.location.search.substr(1).match(a);
+            if(r!=null){return unescape(r[2])}else {return null};
+        },
+        /**
+         * @desc : 判断obj是否为空
+         * @access : public
+         * @param : obj / arr
+         * @return : boolean
+         */
+        isEmptyObject:function(a){var b;for(b in a) {return true}return false;}
+    };
+    window.$OH = $OH;
 })(document, window);
-
-window.onload = function () {
-    var s = document.querySelectorAll('#head a img')[0].height;
-    document.body.style.paddingTop = s +'px';
-};
-
-var $OH = {
-    /**
-     * @desc : 获取url上参数值
-     * @access : public
-     * @param : string
-     * @return : object / null
-     */
-    getQueryStr : function (n) {
-        var re = new RegExp("(^|&)"+ n +"=([^&]*)(&|$)");
-        var r = window.location.search.substr(1).match(re);
-        if(r!=null){return unescape(r[2]);}else{return null;}
-    }
-};
-
-
-
